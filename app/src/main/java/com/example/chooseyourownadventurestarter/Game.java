17d63530ce8c55a74e831d32d4b3f8836c1427a0
butterfield <butterfield@mentorschools.org>
2024-05-29 14:30:10 -0400

    Added properties and methods to the Place class that restrict player movement by setting "exit" directions. By default a Place object has no exits.

--- app/src/main/java/com/example/chooseyourownadventurestarter/Game.java
+++ app/src/main/java/com/example/chooseyourownadventurestarter/Game.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +22,44 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        initializeTheWorld();
+        restrictPlayerMovement();
+
+        storyImageView = findViewById(R.id.storyImageView);
+        storyTextView = findViewById(R.id.storyTextView);
+        storyTextView.setText(playerLocation.toString());
+
+        northBtn = findViewById(R.id.northBtn);
+        northBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move("north");
+            }
+        });
+        southBtn = findViewById(R.id.southBtn);
+        southBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move("south");
+            }
+        });
+        eastBtn = findViewById(R.id.eastBtn);
+        eastBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move("east");
+            }
+        });
+        westBtn = findViewById(R.id.westBtn);
+        westBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                move("west");
+            }
+        });
+    }
+
+    private void initializeTheWorld() {
         String[] row1 = getResources().getStringArray(R.array.row1);
         String[] row2 = getResources().getStringArray(R.array.row2);
         String[] row3 = getResources().getStringArray(R.array.row3);
@@ -29,74 +68,56 @@ public class Game extends AppCompatActivity {
         world = new World(descriptions);
         world.setPlayerPosition(1, 1);
         playerLocation = world.getPlayerLocation();
-
-        storyImageView = findViewById(R.id.storyImageView);
-        storyTextView = findViewById(R.id.storyTextView);
-        storyTextView.setText(playerLocation.getDescription());
-
-        northBtn = findViewById(R.id.northBtn);
-        northBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                move(playerLocation.getRow() - 1, playerLocation.getCol());
-            }
-        });
-        southBtn = findViewById(R.id.southBtn);
-        southBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                move(playerLocation.getRow() + 1, playerLocation.getCol());
-            }
-        });
-        eastBtn = findViewById(R.id.eastBtn);
-        eastBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                move(playerLocation.getRow(), playerLocation.getCol() + 1);
-            }
-        });
-        westBtn = findViewById(R.id.westBtn);
-        westBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                move(playerLocation.getRow(), playerLocation.getCol() - 1);
-            }
-        });
     }
 
-    public void move(int row, int col) {
+    private void restrictPlayerMovement() {
+        // The center position only allows north and south exits.
+        world.getMap()[0][0].setExits(new String[] { "east" });
+        world.getMap()[0][1].setExits(new String[] { "east", "south", "west" });
+        world.getMap()[0][2].setExits(new String[] { "west" });
+        world.getMap()[1][1].setExits(new String[] { "north", "south" });
+        world.getMap()[2][0].setExits(new String[] { "east" });
+        world.getMap()[2][1].setExits(new String[] { "north", "east", "west" });
+        world.getMap()[2][2].setExits(new String[] { "west" });
+    }
+
+    public void move(String direction) {
+        int row = playerLocation.getRow();
+        int col = playerLocation.getCol();
+        if (direction.equalsIgnoreCase("north")) {
+            row--;
+        } else if (direction.equalsIgnoreCase("east")) {
+            col++;
+        }  else if (direction.equalsIgnoreCase("south")) {
+            row++;
+        } else if (direction.equalsIgnoreCase("west")) {
+            col--;
+        }
         boolean invalidRow = row < 0 || row >= world.getMap().length;
         boolean invalidCol = col < 0 || col >= world.getMap()[0].length;
-        if (invalidRow || invalidCol) {
-            String feedback = "Cannot move in that direction.\nYour location is: " + playerLocation;
-            storyTextView.setText(feedback);
-        } else {
+
+        if (playerLocation.canExit(direction) && !invalidRow && !invalidCol) {
             world.setPlayerPosition(row, col);
             playerLocation = world.getPlayerLocation();
             storyTextView.setText(playerLocation.toString());
             updateImage(row, col);
+        } else {
+            storyTextView.setText(playerLocation.toString());
+            Toast.makeText(getApplicationContext(),
+                      "Cannot move in that direction",
+                           Toast.LENGTH_SHORT).show();
         }
     }
 
     public void updateImage(int row, int col) {
-        if (row == 0 && col == 0) {
-            storyImageView.setImageResource(R.drawable.r0c0);
-        } else if (row == 0 && col == 1) {
-            storyImageView.setImageResource(R.drawable.r0c1);
-        } else if (row == 0 && col == 2) {
-            storyImageView.setImageResource(R.drawable.r0c2);
-        } else if (row == 1 && col == 0) {
-            storyImageView.setImageResource(R.drawable.r1c0);
-        } else if (row == 1 && col == 1) {
-            storyImageView.setImageResource(R.drawable.r1c1);
-        } else if (row == 1 && col == 2) {
-            storyImageView.setImageResource(R.drawable.r1c2);
-        } else if (row == 2 && col == 0) {
-            storyImageView.setImageResource(R.drawable.r2c0);
-        } else if (row == 2 && col == 1) {
-            storyImageView.setImageResource(R.drawable.r2c1);
-        } else if (row == 2 && col == 2) {
-            storyImageView.setImageResource(R.drawable.r2c2);
+        int[][] images = {
+                            { R.drawable.r0c0, R.drawable.r0c1, R.drawable.r0c2 },
+                            { R.drawable.r1c0, R.drawable.r1c1, R.drawable.r1c2 },
+                            { R.drawable.r2c0, R.drawable.r2c1, R.drawable.r2c2 },
+        };
+
+        if (row >= 0 && row < images.length && col >= 0 && col < images[0].length) {
+            storyImageView.setImageResource(images[row][col]);
         } else {
             storyImageView.setImageResource(R.drawable.grid);
         }
--- app/src/main/java/com/example/chooseyourownadventurestarter/Place.java
+++ app/src/main/java/com/example/chooseyourownadventurestarter/Place.java
@@ -6,10 +6,13 @@ public class Place {
     private int row;
     private int col;
 
+    private boolean[] exits;    // North, East, South, West
+
     public Place(String description, int row, int col) {
         this.description = description;
         this.row = row;
         this.col = col;
+        exits = new boolean[] { false, false, false, false };
     }
 
     public String getDescription() {
@@ -24,7 +27,40 @@ public class Place {
         return col;
     }
 
+    public void setExits(String[] validDirections) {
+        if (validDirections == null) {
+            throw new IllegalArgumentException("String array cannot be null.");
+        }
+        exits = new boolean[] { false, false, false, false };
+        for (String direction : validDirections) {
+            if (direction.equalsIgnoreCase("north")) {
+                exits[0] = true;
+            } else if (direction.equalsIgnoreCase("east")) {
+                exits[1] = true;
+            } else if (direction.equalsIgnoreCase("south")) {
+                exits[2] = true;
+            } else if (direction.equalsIgnoreCase("west")) {
+                exits[3] = true;
+            }
+        }
+    }
+
+    public boolean canExit(String direction) {
+        if (direction.equalsIgnoreCase("north")) {
+            return exits[0];
+        } else if (direction.equalsIgnoreCase("east")) {
+            return exits[1];
+        } else if (direction.equalsIgnoreCase("south")) {
+            return exits[2];
+        }  else if (direction.equalsIgnoreCase("west")) {
+            return exits[3];
+        } else {
+            return false;
+        }
+    }
+
     public String toString() {
-        return description + "\nRow index: " + row + " Column index: " + col;
+        return "Your position is: (" + row + ", " + col + ")" +
+                "\n\n" + description;
     }
 }
